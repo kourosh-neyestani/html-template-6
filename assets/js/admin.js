f72d757b777f75e3cdced8f1261031c583c14bd9
Kourosh Neyestani <kourosh.neyestani@gmail.com>
2021-03-02 16:33:51 +0330

    Modified: admin.js, admin.css. Added new pricing page

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -34,6 +34,10 @@
     AFRA.Admin_Pricing = function () {
         var selector = $("[data-admin-select-price]").find(".el-select2");
 
+        if (!selector.length) {
+            return;
+        }
+
         selector.on("change", function (e) {
             var output,
                 selected = e.target.value,
@@ -59,6 +63,83 @@
         });
     };
 
+    /*====== Admin: Pricing ======*/
+    AFRA.Admin_Pricing_2 = function () {
+        var price = 0,
+            percent = 20,
+            input = $("[data-admin-select-price-2]").find("input"),
+            selector = $("[data-admin-select-price-2]").find(".el-select2");
+
+        function calc(x, y) {
+            x = parseInt(x);
+            y = parseInt(y);
+
+            if (!x) {
+                return 0;
+            }
+
+            z = (x * y) / 100;
+            return z;
+        }
+
+        input.on("input", function (e) {
+            var value = parseInt(e.target.value);
+            if (!value) {
+                price = 0;
+                $(".select2-selection__rendered").text(0);
+                selector.prop("disabled", true);
+                $("[data-admin-calculate-price-2]").find(".output").text(0);
+                $("[data-admin-calculate-price-id-2='1'], [data-admin-calculate-price-id-2='5'], [data-admin-calculate-price-id-2='10']").text(0);
+                return;
+            } else {
+                selector.prop("disabled", false);
+            }
+            price = value;
+            var p = value + calc(value, percent);
+            $("[data-admin-calculate-price-2='1']").find(".output").text(p).digits();
+            $("[data-admin-calculate-price-2='5']")
+                .find(".output")
+                .text(5 * p)
+                .digits();
+            $("[data-admin-calculate-price-2='10']")
+                .find(".output")
+                .text(10 * p)
+                .digits();
+            $("[data-admin-calculate-price-id-2='1']").text(value).digits();
+            $("[data-admin-calculate-price-id-2='5']")
+                .text(value * 5)
+                .digits();
+            $("[data-admin-calculate-price-id-2='10']")
+                .text(value * 10)
+                .digits();
+        });
+
+        selector.on("change", function (e) {
+            var v = parseInt(e.target.value),
+                d = $(this).parent().parent().parent().data("admin-select-price-2");
+
+            if (d === 5) {
+                var p = 5 * (price + calc(price, percent)),
+                    q = calc(p, v),
+                    o = p - q;
+
+                $("[data-admin-calculate-price-2='5']").find(".output").text(o).digits();
+                $("[data-admin-calculate-price-id-2='5']")
+                    .text(price * 5 - calc(price * 5, v))
+                    .digits();
+            } else if (d === 10) {
+                var p = 10 * price,
+                    q = calc(p, v),
+                    o = p - q;
+
+                $("[data-admin-calculate-price-2='10']").find(".output").text(o).digits();
+                $("[data-admin-calculate-price-id-2='10']")
+                    .text(price * 10 - calc(price * 10, v))
+                    .digits();
+            }
+        });
+    };
+
     /*====== Admin: Multi_Checkboxes ======*/
     AFRA.Admin_Multi_Checkboxes = function () {
         var i = 0,
@@ -74,8 +155,6 @@
         input.on("click", function (e) {
             var li = $(this).parent();
 
-            console.log($(this));
-
             if (i >= limit) {
                 return;
             }
@@ -89,8 +168,6 @@
                 li.removeClass("active");
                 $(this).prop("checked", false);
             }
-
-            console.log(i);
         });
     };
 
@@ -105,6 +182,7 @@
     $(document).ready(function () {
         AFRA.Admin_Tabs();
         AFRA.Admin_Pricing();
+        AFRA.Admin_Pricing_2();
         AFRA.Admin_Multi_Checkboxes();
     });
 })(jQuery);
